24a7d8f63c013ba118c301440d9f3e44cc3798c0
ncornet001 <ncornet001@ensc.fr>
2025-10-02 16:39:34 +0200

    Map.

--- map.py
+++ map.py
@@ -8,7 +8,7 @@ X = [0]
 Y = [0]
 angle = 0
 
-def record_new_position(new_x,new_y,delta_time):
+def record_new_position(new_x,new_y):
     X.append(new_x)
     Y.append(new_y)
 
@@ -18,11 +18,6 @@ def record_new_wheel_movement(ws1,ws2,delta_time):
     angle = new_angle
     X.append(new_x)
     Y.append(new_y)
-
-def record_reset():
-    X = [0]
-    Y = [0]
-    angle = 0
 
 def draw_map():
     plt.plot(X,Y)
